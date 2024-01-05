7052c221afc7aba45f20ed092a6bd84cc7ba282b
david emioma <ue22aac@herts.ac.uk>
2024-01-05 14:32:24 +0000

    Added persist state to use-cart hook

--- hooks/use-cart.ts
+++ hooks/use-cart.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { Product } from "@/payload-types";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type CartItem = {
   product: Product;
@@ -12,15 +13,25 @@ type CartProps = {
   clearCart: () => void;
 };
 
-const useCart = create<CartProps>((set) => ({
-  items: [],
-  addItem: (product) =>
-    set((state) => ({ items: [...state.items, { product }] })),
-  removeItem: (id) =>
-    set((state) => ({
-      items: state.items.filter((item) => item.product.id !== id),
-    })),
-  clearCart: () => set({ items: [] }),
-}));
+const useCart = create<CartProps>()(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (product) =>
+        set((state) => {
+          return { items: [{ product }, ...state.items] };
+        }),
+      removeItem: (id) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.product.id !== id),
+        })),
+      clearCart: () => set({ items: [] }),
+    }),
+    {
+      name: "cart-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export default useCart;
